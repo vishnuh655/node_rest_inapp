4d17163231a5121f125576f0b9ccd4b7a81c3d51
vishnuh655 <vishnuh655@gmail.com>
2022-06-18 23:59:48 +0530

    Added Student Update and Delete APIs

--- src/controllers/v1/student.controller.js
+++ src/controllers/v1/student.controller.js
@@ -1,4 +1,3 @@
-const Joi = require("joi");
 const Student = require("../../models/student.model");
 const schemaValidate = require("../../helpers/schema.helper");
 const StudentSchema = require("../../schemas/student.schema");
@@ -8,7 +7,7 @@ module.exports = {
     try {
       const validatedData = await schemaValidate(StudentSchema, req, res);
       const student = await Student.create(validatedData);
-      res.send(student);
+      res.respondCreated(student, "Student created successfully");
     } catch (err) {
       res.failServerError(err);
     }
@@ -25,12 +24,37 @@ module.exports = {
 
   updateStudent: async (req, res) => {
     try {
-      const students = await Student.update(req.body);
-      res.send(students);
+      const validatedData = await schemaValidate(StudentSchema, req, res);
+      const updatedStudent = await Student.update(validatedData, {
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (updatedStudent && updatedStudent[0] === 1) {
+        res.respondUpdated(updatedStudent, "Student updated successfully");
+      } else {
+        res.failNotFound("Student not found");
+      }
     } catch (err) {
       res.failServerError(err);
     }
   },
 
-  deleteStudent: async (req, res) => {},
+  deleteStudent: async (req, res) => {
+    try {
+      if (
+        await Student.destroy({
+          where: {
+            id: req.params.id,
+          },
+        })
+      ) {
+        res.respondDeleted(true, "Student deleted successfully");
+      } else {
+        res.failNotFound("Student not found");
+      }
+    } catch (err) {
+      res.failServerError(err);
+    }
+  },
 };
--- src/routes/api.v1.route.js
+++ src/routes/api.v1.route.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const schemaValidate = require("../middlewares/schema.middleware");
 const testController = require("../controllers/v1/test.controller");
 const studentController = require("../controllers/v1/student.controller");
-const studentSchema = require("../schemas/student.schema");
 
 const router = express.Router();
 
@@ -10,7 +8,7 @@ router.get("/", testController.getTest);
 
 router.get("/students", studentController.getStudents);
 router.post("/students", studentController.createStudent);
-router.put("/students/{id}", studentController.updateStudent);
-router.delete("/students/{id}", studentController.deleteStudent);
+router.put("/students/:id", studentController.updateStudent);
+router.delete("/students/:id", studentController.deleteStudent);
 
 module.exports = router;
